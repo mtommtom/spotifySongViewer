0e55a4a1dffeb9cd0c11f1c77db07901f5fd1275
mtommtom <makeotom@yahoo.com>
2025-03-19 18:49:53 -0700

    clean main.js

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const { app, BrowserWindow } = require('electron');
-const web = require('./app')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        // fullscreen: true
         width: 1000,
         height: 700
     });
-    // win.maximize();pp
-    // win.show();
     win.loadURL('http://localhost');
 }
 
